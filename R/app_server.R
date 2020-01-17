fe018aae5830ae52ef13e4dad690fc6e3984cb36
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2020-01-17 15:43:26 +0100

    Add example otu dictionary to app_server

--- R/app_server.R
+++ R/app_server.R
@@ -3,5 +3,6 @@ app_server <- function(input, output,session) {
   # Load package data in the session (for testing purpose)
   # data("physeq", package = "affiliationExplorer")
   # data("affi", package = "affiliationExplorer")
+  # data("otu_dictionary", package = "affiliationExplorer")
   # List the first level callModules here
 }
